f23f4ed6c5f4163870a277fb1864ced0e8d8c4f5
Ian Bovard <thebestpantsintown@gmail.com>
2017-06-16 13:42:29 -1000

    math buttons

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,35 +1,48 @@
 const DISPLAY = document.getElementById("screen");
-const BTN9 = document.getElementById("nine");
-const BUTTONBOX = document.getElementById('buttonsBox');
-
-const NUMBERARRAY = ["9","8","7","6","5","4","3","2","1","0","00"];
-
+const BUTTON_BOX = document.getElementById("buttonBox");
+const MATH_BOX = document.getElementById("mathBox");
+const REGISTER_BOX = document.getElementById("registerBox");
+const NUMBER_ARRAY = ["9","8","7","6","5","4","3","2","1","0","00"];
+const MATH_ARRAY = ["+", "-", "x", "/", "="];
+const REGISTER_ARRAY = ["withdraw cash", "deposit cash", "get balance", "clear"];
 
  let displayInput =function(num){
     displayPopulated = false;
-return function(){
-    if (displayPopulated === false){
-      DISPLAY.innerHTML = num;
-      displayPopulated = true;
-    }else{
-      DISPLAY.innerHTML+= num;
-    }
+ return function(){
+      if (displayPopulated === false){
+        DISPLAY.innerHTML = num;
+        displayPopulated = true;
+      }else{
+        DISPLAY.innerHTML+= num;
+     }
   };
 };
-displayInput();
+  displayInput();
 
   let buttonPop = function(array){
     for (let i = 0; i < array.length; i++){
       let numberButtons = document.createElement("button");
-      numberButtons.className = "buttons";
+      numberButtons.className = "numButt";
+      numberButtons.id = array[i];
       numberButtons.value = array[i];
       numberButtons.innerHTML = " [ " + array[i] + " ] ";
       numberButtons.addEventListener("click", displayInput(array[i]));
-      BUTTONBOX.appendChild(numberButtons);
+      BUTTON_BOX.appendChild(numberButtons);
+      console.log(numberButtons.id);
 
     }
   };
-  buttonPop(NUMBERARRAY);
+  buttonPop(NUMBER_ARRAY);
 
+  let mathPop = function(array){
+    for (let i = 0; i < array.length; i++){
+      let mathButtons = document.createElement("button");
+      mathButtons.className = "mathButt";
+      mathButtons.id = array[i];
+      mathButtons.innerHTML = " [ " + array[i] + " ] ";
+      MATH_BOX.appendChild(mathButtons);
+    }
+  };
+  mathPop(MATH_ARRAY);
 
 
