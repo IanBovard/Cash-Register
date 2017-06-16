ac0d86636a37db2039edf959445edab54eed4bb2
Ian Bovard <thebestpantsintown@gmail.com>
2017-06-16 13:50:21 -1000

    more buttons

--- js/cash_register.js
+++ js/cash_register.js
@@ -2,13 +2,13 @@ const DISPLAY = document.getElementById("screen");
 const BUTTON_BOX = document.getElementById("buttonBox");
 const MATH_BOX = document.getElementById("mathBox");
 const REGISTER_BOX = document.getElementById("registerBox");
-const NUMBER_ARRAY = ["9","8","7","6","5","4","3","2","1","0","00"];
+const NUMBER_ARRAY = ["9","8","7","6","5","4","3","2","1","0","00", "."];
 const MATH_ARRAY = ["+", "-", "x", "/", "="];
 const REGISTER_ARRAY = ["withdraw cash", "deposit cash", "get balance", "clear"];
 
- let displayInput =function(num){
+let displayInput = function(num){
     displayPopulated = false;
- return function(){
+  return function(){
       if (displayPopulated === false){
         DISPLAY.innerHTML = num;
         displayPopulated = true;
@@ -19,7 +19,7 @@ const REGISTER_ARRAY = ["withdraw cash", "deposit cash", "get balance", "clear"]
 };
   displayInput();
 
-  let buttonPop = function(array){
+let buttonPop = function(array){
     for (let i = 0; i < array.length; i++){
       let numberButtons = document.createElement("button");
       numberButtons.className = "numButt";
@@ -34,15 +34,24 @@ const REGISTER_ARRAY = ["withdraw cash", "deposit cash", "get balance", "clear"]
   };
   buttonPop(NUMBER_ARRAY);
 
-  let mathPop = function(array){
-    for (let i = 0; i < array.length; i++){
-      let mathButtons = document.createElement("button");
-      mathButtons.className = "mathButt";
-      mathButtons.id = array[i];
-      mathButtons.innerHTML = " [ " + array[i] + " ] ";
-      MATH_BOX.appendChild(mathButtons);
-    }
-  };
-  mathPop(MATH_ARRAY);
+let mathPop = function(array){
+  for (let i = 0; i < array.length; i++){
+    let mathButtons = document.createElement("button");
+    mathButtons.className = "mathButt";
+    mathButtons.id = array[i];
+    mathButtons.innerHTML = " [ " + array[i] + " ] ";
+    MATH_BOX.appendChild(mathButtons);
+  }
+};
+mathPop(MATH_ARRAY);
 
-
+let registerPop = function(array){
+  for (let i = 0; i < array.length; i++){
+    let registerButtons = document.createElement("button");
+    registerButtons.className = "mathButt";
+    registerButtons.id = array[i];
+    registerButtons.innerHTML = " [ " + array[i] + " ] ";
+    REGISTER_BOX.appendChild(registerButtons);
+  }
+};
+registerPop(REGISTER_ARRAY);
