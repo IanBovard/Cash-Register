054b06890d922b53b91ad02d5624749e056c0d1e
Ian Bovard <thebestpantsintown@gmail.com>
2017-06-16 19:34:53 -1000

    add workingish

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 //html variables and arrays
 const DISPLAY = document.getElementById("screen");
 const BUTTON_BOX = document.getElementById("buttonBox");
@@ -6,9 +7,9 @@ const REGISTER_BOX = document.getElementById("registerBox");
 const NUMBER_ARRAY = ["9","8","7","6","5","4","3","2","1","0","00"];
 const MATH_ARRAY = ["+", "-", "x", "/", "=", "."];
 const REGISTER_ARRAY = ["withdraw cash", "deposit cash", "get balance", "clear"];
-const CALC = window.calculator;
+let CALC = window.calculator;
 let displayMem = 0;
-//three event listeners
+
 let numberInput = function(num){
     displayPopulated = false;
   return function(){
@@ -20,8 +21,7 @@ let numberInput = function(num){
      }
   };
 };
-  numberInput();
-//button population arrays
+
 let buttonPop = function(array){
     for (let i = 0; i < array.length; i++){
       let numberButtons = document.createElement("button");
@@ -31,30 +31,62 @@ let buttonPop = function(array){
       numberButtons.innerHTML = " [ " + array[i] + " ] ";
       numberButtons.addEventListener("click", numberInput(array[i]));
       BUTTON_BOX.appendChild(numberButtons);
-      console.log(numberButtons.id);
 
     }
   };
   buttonPop(NUMBER_ARRAY);
 
 let mathPop = function(array){
+
   for (let i = 0; i < array.length; i++){
     let mathButtons = document.createElement("button");
     mathButtons.className = "mathButt";
     mathButtons.id = array[i];
     mathButtons.innerHTML = " [ " + array[i] + " ] ";
-    mathButtons.addEventListener("click", function(){
-  let input = 0;
-  input = DISPLAY.innerHTML;
-  if (mathButtons.innerHTML === " [ x ] "){
-    DISPLAY.innerHTML = "0.00";
-
-  }
-});
     MATH_BOX.appendChild(mathButtons);
+
   }
 };
 mathPop(MATH_ARRAY);
+   let input = 0;
+   let total = 0;
+    let storedOp;
+let mathAdd = document.getElementById("+");
+  mathAdd.addEventListener("click", function(){
+    if (typeof storedOp !== "string"){
+      input = parseInt(DISPLAY.innerHTML);
+      total += input;
+      storedOp = mathAdd.innerHTML;
+      DISPLAY.innerHTML = input;
+      displayPopulated = false;
+      console.log(input);
+      console.log(storedOp);
+    }else{
+      input = parseInt(DISPLAY.innerHTML);
+      total+=input;
+      DISPLAY.innerHTML = total;
+      displayPopulated = false;
+      console.log(total);
+      console.log(storedOp);
+      console.log(input);
+    }
+  });
+
+
+/*mathButtons.addEventListener("click", function(){
+  let input = 0;
+  let storedOp;
+  if (typeof mathButtons.id !== array[i]){
+    input = DISPLAY.innerHTML;
+    storedOp = mathButtons.innerHTML;
+    DISPLAY.innerHTML = "0.00";
+    displayPopulated = false;
+    console.log(input);
+    console.log(storedOp);
+  }else if (mathButtons.id === "0"){
+    DISPLAY.innerHTML = "BLAH";
+  }
+});*/
 
 let registerPop = function(array){
   for (let i = 0; i < array.length; i++){
@@ -65,6 +97,8 @@ let registerPop = function(array){
     registerButtons.addEventListener('click', function(){
       DISPLAY.innerHTML = "0.00";
       displayPopulated = false;
+      storedOp = 0;
+      total = 0;
     });
     REGISTER_BOX.appendChild(registerButtons);
 
